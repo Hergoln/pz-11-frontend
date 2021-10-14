38787f123c36dd30d1c4dae16e5dd09b09d0d804
Maciej <gorski.maciej@o2.pl>
2021-10-14 20:35:08 +0200

    set up linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { JoinGameModal } from "./components/JoinGameModal";
 
 function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setModalVisible(true)}>
+        Show the fokin' modal
+      </button>
+      <JoinGameModal
+        onJoinGame={() => {}}
+        onCancel={() => setModalVisible(false)}
+        modalProps={{ visible: modalVisible, closable: false }}
+      />
     </div>
   );
 }

ced13019a2ea9ca27ed05a1c5b64ec2d78042e44
Maciej <gorski.maciej@o2.pl>
2021-10-24 22:21:46 +0200

    added env template file, added working WebsocketHandler component

--- src/components/WebsocketHandler/index.tsx
+++ src/components/WebsocketHandler/index.tsx
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { w3cwebsocket as Websocket } from 'websocket';
+import { w3cwebsocket as Websocket, IMessageEvent, ICloseEvent } from 'websocket';
 
 interface WebsocketProps {
     gameId: string;
-    onMessageReceived?: () => void;
+    onMessageReceived: (event: IMessageEvent) => void;
+    onConnectionClosed?: (event: ICloseEvent) => void;
+    onError?: (error: Error) => void;
 }
 
 type Connection = Websocket | null;
 
-export const WebsocketHandler = ({ gameId, onMessageReceived }: WebsocketProps) => {
+export const WebsocketHandler = ({ gameId, onMessageReceived, onConnectionClosed, onError }: WebsocketProps) => {
 
     const [connection, setConnection] = useState<Connection>(null);
 
     useEffect(() => {
         if (connection === null) {
-            //@ts-ignore
-            setConnection(new Websocket(process.env.API_SERVER_URL));
+            console.log(`${process.env.REACT_APP_API_WEBSOCKET_SERVER_URL}:${process.env.REACT_APP_API_SERVER_PORT}`);
+            const socket = new Websocket(`${process.env.REACT_APP_API_WEBSOCKET_SERVER_URL}:${process.env.REACT_APP_API_SERVER_PORT}`);
+            socket.onmessage = onMessageReceived;
+            // @ts-ignore
+            socket.onclose = onConnectionClosed;
+            // @ts-ignore
+            socket.onerror = onError;
+            setConnection(socket);
         }
 
+        //returned function cleans up resources used/set up during useEffect
         return () => {
             connection?.close();
         };
-    }, [connection]);
+    }, [connection, onMessageReceived, onConnectionClosed, onError]);
 
     return (<></>);
 };

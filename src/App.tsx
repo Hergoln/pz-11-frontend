f450963a205ea86fd2e1ee933d5e6dffede8ea15
BigDinDonMan <34507893+BigDinDonMan@users.noreply.github.com>
2021-10-31 17:09:49 +0100

    Websocket integration (#2)
    * added websocket dependency, added empty WebsocketHandler class, added env-template
    
    * converted websocket handler to function component
    
    * added env template file, added working WebsocketHandler component
    
    * code cleanup & working keyboard press detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 //@ts-ignore
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import { InfoPage } from './pages/InfoPage';
+import { IMessageEvent, ICloseEvent } from 'websocket';
+import { WebsocketHandler } from './components/WebsocketHandler';
+
+function messageHandler(event: IMessageEvent) {
+  console.log("got a message: " + event.data);
+}
+
+function closeHandler(event: ICloseEvent) {
+  console.log("closing the connection: " + event.code);
+}
+
+function errorHandler(error: Error) {
+  console.log(error);
+}
 
 function App() {
   return (
     <div className="page-container">
+      <WebsocketHandler gameId={''} onMessageReceived={messageHandler} onConnectionClosed={closeHandler} onError={errorHandler} />
       <div className='content'>
         <Router>
           <Switch>
